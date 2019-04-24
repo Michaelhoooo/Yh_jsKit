40aabb06fc4b042ff0eaffdfc6242edcddfc38f6
何子豪 <zhe2@xiaohongshu.com>
2019-04-24 16:32:16 +0800

    update

--- 日历组件.js
+++ 日历组件.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * 有bug，待修改
+ * @param {*} year
+ * @param {*} month
+ * @returns
+ */
 function getList(year, month){
   let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
